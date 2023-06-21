89df51a9b518c237851c1a70a101d69c59dbb64d
nihid007 <nihidkannur@gmail.com>
2023-06-21 12:35:32 +0530

    new comitddd

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import{Routes,Route} from "react-router-dom";
+import Register from './pages/Register';
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+    <Routes>
+     
+    <Route path="/" exact element={<Register />} />
+
+    </Routes>
+      
+    </>
+  )
+} 
+
+export default App
